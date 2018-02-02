1cdc30e2940355538301639dbbe0e78413ac93c9
Sergey Nechaev <sergey.nechaev@equifax.com>
2018-02-02 12:16:37 +1100

    Unit test fixed

--- src/test/java/io/zdp/client/TestZdpTransfers.java
+++ src/test/java/io/zdp/client/TestZdpTransfers.java
@@ -9,6 +9,7 @@ import io.zdp.api.model.BalanceResponse;
 import io.zdp.api.model.TransferResponse;
 import io.zdp.client.impl.ZdpClientImpl;
 import io.zdp.common.crypto.CryptoUtils;
+import io.zdp.common.crypto.Signer;
 import junit.framework.TestCase;
 
 public class TestZdpTransfers extends TestCase {
@@ -24,12 +25,12 @@ public class TestZdpTransfers extends TestCase {
 
 		System.out.println(zdp.getFee());
 
-		String seed1 = "116b35a304b3c4607e33d92958ab7ace5ca246bf16b43ae63bce9526caea73c4";
+		String seed1 = "1111111111111111111111111111111111111111111111111111111111111111";
 		KeyPair keys1 = CryptoUtils.generateKeys(seed1);
 		BalanceResponse resp1 = zdp.getAddressBalance(keys1.getPublic().getEncoded(), keys1.getPrivate().getEncoded());
 		System.out.println(resp1.getBalance());
 
-		String seed2 = "226b35a304b3c4607e33d92958ab7ace5ca246bf16b43ae63bce9526caea73c4";
+		String seed2 = "2222222222222222222222222222222222222222222222222222222222222222";
 		KeyPair keys2 = CryptoUtils.generateKeys(seed2);
 		BalanceResponse resp2 = zdp.getAddressBalance(keys2.getPublic().getEncoded(), keys2.getPrivate().getEncoded());
 		System.out.println(resp2.getBalance());
@@ -37,7 +38,7 @@ public class TestZdpTransfers extends TestCase {
 		// 1 -> 2 50 coins
 		BigDecimal amount = BigDecimal.valueOf(50);
 
-		TransferResponse resp = zdp.transfer(keys1.getPublic().getEncoded(), keys1.getPrivate().getEncoded(), seed2, amount, "ref1", "to ref2");
+		TransferResponse resp = zdp.transfer(keys1.getPublic().getEncoded(), keys1.getPrivate().getEncoded(), Signer.getPublicKeyHash(keys2.getPublic()), amount, "ref1", "to ref2");
 
 		System.out.println(resp);
 
